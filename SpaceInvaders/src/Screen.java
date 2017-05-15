0df694e6192e3b11034fba566d04466a9df0ed7c
takahiromollenkamp <takahiro.mollenkamp@gmail.com>
2017-05-15 09:27:09 -0700

    idk

--- SpaceInvaders/src/Screen.java
+++ SpaceInvaders/src/Screen.java
@@ -1,7 +1,7 @@
 import java.awt.Container;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
+
 
 
 public class Screen {
@@ -10,12 +10,14 @@ public class Screen {
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    Container contentPane = frame.getContentPane();
 
-	    // Add a close button
-	    JButton closeButton = new JButton("Close");
-	    contentPane.add(closeButton);
-
+	    
+	    frame.setSize(700,700);
+	    
+	    frame.setLocationRelativeTo(null);
 	    // set the size of the frame 300 x 200
-	    frame.pack();
+
+	    
+	       
 	    frame.setVisible(true);
 
 		
